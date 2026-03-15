67dcd683ccffb7e26d4f022c74ccdc44d519e040
ViralJain758 <viraljain758@gmail.com>
2026-03-16 03:33:14 +0530

    Initial project setup with backend and frontend folders

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import connectDB from "./src/config/db.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Taskzen API running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
