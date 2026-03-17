0eb6d38019ea53207d78dd4617cd3668c52b4860
ViralJain758 <viraljain758@gmail.com>
2026-03-17 23:57:34 +0530

    feat: add project page route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import WorkspacePage from "./pages/WorkspacePage.tsx";
+import ProjectPage from "./pages/ProjectPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/workspaces/:workspaceId" element={<WorkspacePage />} />
+        <Route path="/project/:projectId" element={<ProjectPage />} />
       </Routes>
     </BrowserRouter>
   );
